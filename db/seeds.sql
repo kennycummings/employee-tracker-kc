d7854dcc2ef044610d57d2c68e71c0b7cc9cf356
kennycummings <kennethccummings@gmail.com>
2024-01-04 21:45:00 -0500

    Debugged code in .sql files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+-- Insert department names into department table
 INSERT INTO department (name)
 VALUES  
     ("Engineering"),
@@ -5,6 +6,7 @@ VALUES
     ("Legal"),
     ("Sales");
 
+-- Insert role fields into role table
 INSERT INTO roles (title, salary, department_id)
 VALUES  
     ("Sales Lead", 60000, 4),
@@ -16,6 +18,7 @@ VALUES
     ("Legal Team Lead", 155000, 3),
     ("Lawyer", 180000, 3);
 
+-- Insert employee  fields into employee table
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES  
     ("John", "Smith", 1, NULL),
